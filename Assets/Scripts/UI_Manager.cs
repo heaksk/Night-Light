f55fba42d61185d385e9eb05d56c428a27f379c6
Utsjoshua <Joshua.E.Isaraela@student.uts.edu.au>
2023-04-11 21:24:25 +1000

    Added Title Scene with main menu
    The Title scene is added with the main menu. The buttons work which takes you to the levels and tutorial scenes.

---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+
+    private static GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (obj == null){
+            obj = gameObject;
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode){
+
+        if (scene.buildIndex == 0){
+
+            GameObject TutorialButton = GameObject.FindGameObjectWithTag("TutorialButton");
+            Button Tutorial = TutorialButton.GetComponent<Button>();
+		    Tutorial.onClick.AddListener(LoadTutorial);
+
+            GameObject Level1Button = GameObject.FindGameObjectWithTag("Level1Button");
+            Button Level1 = Level1Button.GetComponent<Button>();
+		    Level1.onClick.AddListener(LoadLevel1);
+
+            GameObject Level2Button = GameObject.FindGameObjectWithTag("Level2Button");
+            Button Level2 = Level2Button.GetComponent<Button>();
+		    Level2.onClick.AddListener(LoadLevel2);
+
+            GameObject Level3Button = GameObject.FindGameObjectWithTag("Level3Button");
+            Button Level3 = Level3Button.GetComponent<Button>();
+		    Level3.onClick.AddListener(LoadLevel3);
+        }
+    }
+
+    public void LoadTitle(){
+        SceneManager.LoadScene(0);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadTutorial(){
+        SceneManager.LoadScene(1);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadLevel1(){
+        SceneManager.LoadScene(2);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadLevel2(){
+        SceneManager.LoadScene(3);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadLevel3(){
+        SceneManager.LoadScene(4);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
