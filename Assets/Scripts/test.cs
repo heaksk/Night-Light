d78ee01b516d22aa6846985a940e4a138bb36ef1
heaksk <bh295799@gmail.com>
2023-04-16 23:28:35 +1000

    wall-voice

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+    [Header("UI ietem")]
+    public Text textLabel;
+    public Image faceImage;
+
+    [Header("Test folder")]
+    public TextAsset textFile;
+    public int index;
+
+    List<string> textList = new List<string>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetTextFromFile(textFile);
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            textLabel.text = textList[index];
+            index++;
+        }
+    }
+    void GetTextFromFile(TextAsset file)
+    {
+        textList.Clear();
+        index = 0;
+
+        var lineDate = file.text.Split('\n');
+
+        foreach(var line in lineDate)
+        {
+            textList.Add(line);
+        }
+    }
+}
