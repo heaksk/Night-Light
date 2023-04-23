5a7c45eeaff9479476b201593473cd43329b1f6a
heaksk <bh295799@gmail.com>
2023-04-23 23:04:14 +1000

    book

---
+++ Assets/Scripts/talkUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class talkUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Button;
+    public GameObject taklUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Button.SetActive(true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Button.SetActive(false);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Button.activeSelf && Input.GetKeyDown(KeyCode.Space))
+        {
+            taklUI.SetActive(true);
+        }
+    }
+}
