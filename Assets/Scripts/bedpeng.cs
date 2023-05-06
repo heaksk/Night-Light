7410f0566872284127e23460802f2fc4d42cbfca
Utsjoshua <Joshua.E.Isaraela@student.uts.edu.au>
2023-05-06 16:00:05 +1000

    Comment out Some voices

--- Assets/Scripts/bedpeng.cs
+++ Assets/Scripts/bedpeng.cs
@@ -6,15 +6,15 @@ public class bedpeng : MonoBehaviour
 {
     public AudioSource audioSource;
 
-    public AudioClip bed;
-    public AudioClip wall;
-    public AudioClip clock;
-    public AudioClip sofa;
-    public AudioClip cabinet;
-    public AudioClip shelf;
-    public AudioClip book;
-    public AudioClip table;
-    public AudioClip chair;
+    // public AudioClip bed;
+    // public AudioClip wall;
+    // public AudioClip clock;
+    // public AudioClip sofa;
+    // public AudioClip cabinet;
+    // public AudioClip shelf;
+    // public AudioClip book;
+    // public AudioClip table;
+    // public AudioClip chair;
     public AudioClip door;
     public AudioClip key;
 
@@ -23,33 +23,33 @@ public class bedpeng : MonoBehaviour
         print(collision.gameObject.name);
         switch(collision.gameObject.tag)
         {
-            case "wall":
-            audioSource.clip = wall;
-                break;
-            case "bed":
-                audioSource.clip = bed;
-                break;
-            case "clock":
-                audioSource.clip = clock;
-                break;
-            case "sofa":
-                audioSource.clip = sofa;
-                break;
-            case "cabinet":
-                audioSource.clip = cabinet;
-                break;
-            case "shelf":
-                audioSource.clip = shelf;
-                break;
-            case "book":
-                audioSource.clip = book;
-                break;
-            case "table":
-                audioSource.clip = table;
-                break;
-            case "chair":
-                audioSource.clip = chair;
-                break;
+            // case "wall":
+            // audioSource.clip = wall;
+            //     break;
+            // case "bed":
+            //     audioSource.clip = bed;
+            //     break;
+            // case "clock":
+            //     audioSource.clip = clock;
+            //     break;
+            // case "sofa":
+            //     audioSource.clip = sofa;
+            //     break;
+            // case "cabinet":
+            //     audioSource.clip = cabinet;
+            //     break;
+            // case "shelf":
+            //     audioSource.clip = shelf;
+            //     break;
+            // case "book":
+            //     audioSource.clip = book;
+            //     break;
+            // case "table":
+            //     audioSource.clip = table;
+            //     break;
+            // case "chair":
+            //     audioSource.clip = chair;
+            //     break;
             case "door":
                 audioSource.clip = door;
                 break;
